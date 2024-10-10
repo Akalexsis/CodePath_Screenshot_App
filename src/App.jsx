0a344e62e810b201f5d1717174e1305619914aa7
kaylathornton55@icloud.com <kayla_thornton@Kaylas-MacBook-Air.local>
2024-10-10 15:57:18 -0400

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import APIForm from './components/APIForm'
 import './App.css'
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
-  
+  //  elements user wants to include in screenshot
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
   return (
     <div></div>
   )
